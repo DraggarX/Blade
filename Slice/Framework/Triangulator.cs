1572c77282b753eec44ddfeeaeb7a80ccbaa0256
Железняк Емельян Владиславович <118697996+DraggarX@users.noreply.github.com>
2024-04-20 18:35:54 +0300

    Update Triangulator.cs

--- Slice/Framework/Triangulator.cs
+++ Slice/Framework/Triangulator.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace EzySlice {
+namespace Slice {
 
     /**
      * Contains static functionality for performing Triangulation on arbitrary vertices.
